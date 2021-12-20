9f15e462c764305f63ca28c0455cc8c86c1b0d8e
Aravinda Kumar <aravind.kumar@bootlabstech.om>
2021-12-21 01:12:41 +0530

    added few more iam policies

--- iam.tf
+++ iam.tf
@@ -12,12 +12,33 @@ data "google_project" "service_project" {
   project_id = var.project_id
 }
 
+// project level access
+// https://cloud.google.com/kubernetes-engine/docs/how-to/cluster-shared-vpc
 resource "google_project_iam_binding" "project" {
   count = local.if_create
   project = data.google_project.host_project[0].project_id
   role    = "roles/container.hostServiceAgentUser"
 
   members = [
-    "serviceAccount:service-${data.google_project.service_project[0].number}@container-engine-robot.iam.gserviceaccount.com",
+    "serviceAccount:service-${data.google_project.service_project[0].project_id}@container-engine-robot.iam.gserviceaccount.com",
   ]
+}
+
+
+// network access
+// https://cloud.google.com/kubernetes-engine/docs/how-to/cluster-shared-vpc
+resource "google_compute_subnetwork_iam_member" "cloudservices" {
+  count = local.if_create
+  project = data.google_project.host_project[0].project_id
+  subnetwork = var.subnet
+  role       = "roles/compute.networkUser"
+  member     = "serviceAccount:${data.google_project.service_project[0].project_id}@cloudservices.gserviceaccount.com"
+}
+
+resource "google_compute_subnetwork_iam_member" "container_engine_robot" {
+  count = local.if_create
+  project = data.google_project.host_project[0].project_id
+  subnetwork = var.subnet
+  role       = "roles/compute.networkUser"
+  member     = "serviceAccount:service-${data.google_project.service_project[0].project_id}@container-engine-robot.iam.gserviceaccount.com"
 }
