4e482aacdeb2a0930c91b6ddeea0104301b7c53c
Aravinda Kumar <aravind.kumar@bootlabstech.om>
2021-12-23 22:38:32 +0530

    adding initial node count

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ resource "google_container_node_pool" "primary_node_pool" {
   name       = "${var.name}-primary-node-pool"
   location   = var.location
   cluster    = google_container_cluster.primary.name
+  initial_node_count = 1
 
   autoscaling {
     min_node_count = var.default_node_pool_min_count
@@ -65,6 +66,7 @@ resource "google_container_node_pool" "secondary_node_pool" {
   name       = "${var.name}-secondary-node-pool"
   location   = var.location
   cluster    = google_container_cluster.primary.name
+  initial_node_count = 1
 
   autoscaling {
     min_node_count = var.secondary_node_pool_min_count
