e91b7e8512c3e216215b05b382b360496c68eeda
Aravinda Kumar <aravind.kumar@bootlabstech.om>
2021-12-23 22:14:16 +0530

    removing default np, adding sa

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "google_service_account" "default" {
+  account_id   = "${var.name}-sa"
+  display_name = "${var.name}-sa"
+}
+
 resource "google_container_cluster" "primary" {
   project = var.project_id
   name = "${var.name}-${var.cluster_postfix}"
@@ -16,29 +21,8 @@ resource "google_container_cluster" "primary" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  // remove_default_node_pool = true
-  // initial_node_count       = 1
-
-  node_pool {
-    project = var.project_id
-    name       = "${var.name}-primary-node-pool"
-    location   = var.location
-    cluster    = google_container_cluster.primary.name
-
-    autoscaling {
-      min_node_count = var.default_node_pool_min_count
-      max_node_count = var.default_node_pool_max_count
-    }
-
-    node_config {
-      machine_type = var.machine_type
-
-      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-      oauth_scopes    = [
-        "https://www.googleapis.com/auth/cloud-platform"
-      ]
-    }
-  }
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
   private_cluster_config {
     enable_private_nodes    = var.enable_private_cluster
@@ -53,26 +37,28 @@ resource "google_container_cluster" "primary" {
   ]
 }
 
-// resource "google_container_node_pool" "primary_node_pool" {
-//   project = var.project_id
-//   name       = "${var.name}-primary-node-pool"
-//   location   = var.location
-//   cluster    = google_container_cluster.primary.name
+resource "google_container_node_pool" "primary_node_pool" {
+  project = var.project_id
+  name       = "${var.name}-primary-node-pool"
+  location   = var.location
+  cluster    = google_container_cluster.primary.name
+  initial_node_count
 
-//   autoscaling {
-//     min_node_count = var.default_node_pool_min_count
-//     max_node_count = var.default_node_pool_max_count
-//   }
+  autoscaling {
+    min_node_count = var.default_node_pool_min_count
+    max_node_count = var.default_node_pool_max_count
+  }
 
-//   node_config {
-//     machine_type = var.machine_type
+  node_config {
+    service_account = google_service_account.default.email
+    machine_type = var.machine_type
 
-//     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-//     oauth_scopes    = [
-//       "https://www.googleapis.com/auth/cloud-platform"
-//     ]
-//   }
-// }
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
 
 resource "google_container_node_pool" "secondary_node_pool" {
   project = var.project_id
@@ -86,6 +72,7 @@ resource "google_container_node_pool" "secondary_node_pool" {
   }
 
   node_config {
+    service_account = google_service_account.default.email
     preemptible  = var.preemptible
     machine_type = var.machine_type
 
