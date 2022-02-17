1c755233e5659b651b19b9cfe020f30b2c4bc41c
Silpashree1 <94901964+Silpashree1@users.noreply.github.com>
2022-02-17 17:21:43 +0530

    updated for autocomplete-2

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "location" {
   type        = string
   description = <<-EOT
   {
-   "type": "api",
+   "type": "json",
    "purpose": "autocomplete",
    "data":["asia-east1",
         "asia-east2",
@@ -166,7 +166,125 @@ variable "secondary_node_pool_max_count" {
 
 variable "machine_type" {
   type        = string
-  description = "list of avaliable machine types "
+  description = <<-EOT
+  {
+   "type": "json",
+   "purpose": "autocomplete",
+   "data": [ "c2-standard-16",    
+             "c2-standard-30",
+             "c2-standard-4",
+             "c2-standard-60",
+             "c2-standard-8",
+             "e2-highcpu-16",
+             "e2-highcpu-2",
+             "e2-highcpu-32",
+             "e2-highcpu-4",
+             "e2-highcpu-8",
+             "e2-highmem-16",
+             "e2-highmem-2",
+             "e2-highmem-4",
+             "e2-highmem-8",
+             "e2-medium",
+             "e2-micro",
+             "e2-small",
+             "e2-standard-16",
+             "e2-standard-2",
+             "e2-standard-32",
+             "e2-standard-4",
+             "e2-standard-8",
+             "f1-micro",
+             "g1-small",
+             "m1-megamem-96",
+             "m1-ultramem-160",
+             "m1-ultramem-40",
+             "m1-ultramem-80",
+             "m2-megamem-416",
+             "m2-ultramem-208",
+             "m2-ultramem-416",
+             "n1-highcpu-16",
+             "n1-highcpu-2",
+             "n1-highcpu-32",
+             "n1-highcpu-4",
+             "n1-highcpu-64",
+             "n1-highcpu-8",
+             "n1-highcpu-96",
+             "n1-highmem-16",
+             "n1-highmem-2",
+             "n1-highmem-32",
+             "n1-highmem-4",
+             "n1-highmem-64",
+             "n1-highmem-8",
+             "n1-highmem-96",
+             "n1-standard-1",
+             "n1-standard-16",
+             "n1-standard-2",
+             "n1-standard-32",
+             "n1-standard-4",
+             "n1-standard-64",
+             "n1-standard-8",
+             "n1-standard-96",
+             "n1-ultramem-160",
+             "n1-ultramem-40",
+             "n1-ultramem-80",
+             "n2-highcpu-16",
+             "n2-highcpu-2",
+             "n2-highcpu-32",
+             "n2-highcpu-4",
+             "n2-highcpu-48",
+             "n2-highcpu-64",
+             "n2-highcpu-8",
+             "n2-highcpu-80",
+             "n2-highmem-16",
+             "n2-highmem-2",
+             "n2-highmem-32",
+             "n2-highmem-4",
+             "n2-highmem-48",
+             "n2-highmem-64",
+             "n2-highmem-8",
+             "n2-highmem-80",
+             "n2-standard-16",
+             "n2-standard-2",
+             "n2-standard-32",
+             "n2-standard-4",
+             "n2-standard-48",
+             "n2-standard-64",
+             "n2-standard-8",
+             "n2-standard-80",
+             "n2d-highcpu-128",
+             "n2d-highcpu-16",
+             "n2d-highcpu-2",
+             "n2d-highcpu-224",
+             "n2d-highcpu-32",
+             "n2d-highcpu-4",
+             "n2d-highcpu-48",
+             "n2d-highcpu-64",
+             "n2d-highcpu-8",
+             "n2d-highcpu-80",
+             "n2d-highcpu-96",
+             "n2d-highmem-16",
+             "n2d-highmem-2",
+             "n2d-highmem-32",
+             "n2d-highmem-4",
+             "n2d-highmem-48",
+             "n2d-highmem-64",
+             "n2d-highmem-8",
+             "n2d-highmem-80",
+             "n2d-highmem-96",
+             "n2d-standard-128",
+             "n2d-standard-16",
+             "n2d-standard-2",
+             "n2d-standard-224",
+             "n2d-standard-32",
+             "n2d-standard-4",
+             "n2d-standard-48",
+             "n2d-standard-64",
+             "n2d-standard-8",
+             "n2d-standard-80",
+             "n2d-standard-96"
+            ],
+   "description": "The machine type to create."
+}
+EOT
 }
 
 variable "project_id" {
@@ -178,13 +296,13 @@ variable  "preemptible" {
   type        = bool
   description = <<-EOT
   {
-   "type": "bool",
+   "type": "json",
    "purpose": "autocomplete",
    "data": [
   "true",
   "false"
    ],
-   "default":true
+   "default":true,
    "description": "if set to true, the secondary node pool will be preemptible nodes"
 }
 EOT
@@ -213,7 +331,7 @@ variable "enable_private_cluster" {
   type        = bool
   description = <<-EOT
   {
-   "type": "bool",
+   "type": "json",
    "purpose": "autocomplete",
    "data": [
   "true",
@@ -229,9 +347,9 @@ EOT
 
 variable "is_shared_vpc" {
   type        = bool
-  description =<<-EOT
+  description = <<-EOT
   {
-   "type": "bool",
+   "type": "json",
    "purpose": "autocomplete",
    "data": [
   "true",
@@ -265,7 +383,7 @@ variable "subnet_region" {
   type        = string
   description =  <<-EOT
   {
-   "type": "api",
+   "type": "json",
    "purpose": "autocomplete",
    "data":[ "asia-east1",
         "asia-east2",
