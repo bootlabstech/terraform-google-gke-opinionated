970ca9336924070bbe0f8481d4b8b3cffc16626c
Silpashree Acharya <silpashree@bootlabstech.com>
2024-09-19 16:40:50 +0530

    sec admin role changed

--- iam.tf
+++ iam.tf
@@ -24,7 +24,7 @@ resource "google_project_iam_member" "project" {
 resource "google_project_iam_member" "securityadmin" {
   count   = local.if_create
   project = data.google_project.host_project[0].project_id
-  role    = "roles/compute.securityAdmin"
+  role    = "roles/Custom_compute_security"
   member  = "serviceAccount:service-${data.google_project.service_project[0].number}@container-engine-robot.iam.gserviceaccount.com"
 }
 
